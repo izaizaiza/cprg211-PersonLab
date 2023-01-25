926a11dc230a4eeacc0bacb10aaf82a204dafd94
iza lumpio <iza.lumpio@edu.sait.ca>
2023-01-25 13:23:49 -0700

    made one whole foreach loop for parsing the list

--- PersonLab/Program.cs
+++ PersonLab/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,7 +26,7 @@ namespace PersonLab
 
 
 
-        //display Gina's info
+            //display Gina's info
             Console.WriteLine(secondPerson.DisplayPersonInfo());
             //Display Mike's info as a list
             Console.WriteLine(thirdPerson.ToString());
@@ -35,80 +36,96 @@ namespace PersonLab
             // display mary's age after ten years
             Console.WriteLine("Mary Beals' Age in 10 years is:" + fourthPerson.GetAgeInTenYears());
 
-            // create two relation objects that show Gina and Mary are sisters
+            // skipped the relationship part and jumped to making a list and using that list to print out:
+            // ave age of all the people in the list, who's the youngest and oldest, who has a first name starting with the letter M and who likes the colour blue
 
-            // and that Ian and Mike are brothers.
-            // then display both relationships
-
-            //add all person objects to a list, and then use the list to
+            // first make the list
             List<Person> personList = new List<Person>
             {
                 firstPerson, secondPerson, thirdPerson, fourthPerson
             };
 
-            // just checking if the list works
-            //foreach (Person person in personList)
-            //{
-            //    Console.WriteLine($"{person.DisplayPersonInfo()}");
-            //}
-            
-            // ave age of the people in the list
-            
-            foreach (Person person in personList)
-            {
-               ageSum += person.Age;
-            }
-            ageAve = ageSum / personList.Count; //[1]
-            Console.WriteLine("the average age of the people in the list is "+ageAve.ToString());
-            // youngest person and oldest person
-            // start with a base age say 1;
+         
 
-            // set the first person as the base comparison FOR NOW; will be changed later as foreach loop occurs
+            // set the first person as the base comparison of age FOR NOW; will be changed later as foreach loop occurs
             int ageMin = personList[0].Age;
             int ageMax = personList[0].Age;
             string youngestPerson = "";
             string oldestPerson = "";
+           
+            //need a storage for names that start with M
+            List<string> namesMList = new List<string>();
+            // storage for those who like blue
+            List<string> likesBlueList = new List<string>();
             
+            
+            // now do a foreach loop that goes through each person and save/record the desired info
             foreach (Person person in personList)
             {
+                //add all the ages to be used for ave. computation later
+                ageSum += person.Age;
+
+                // compare the ages of the people, saving only the youngest and olde
                 if (person.Age <= ageMin)
                 {
                     ageMin = person.Age;
-                    youngestPerson= $"{person.FirstName} {person.LastName}";
-                
+                    youngestPerson = $"{person.FirstName} {person.LastName}";
+
                 }
                 else if (person.Age > ageMax)
                 {
                     ageMax = person.Age;
-                    oldestPerson= $"{person.FirstName} {person.LastName}";
+                    oldestPerson = $"{person.FirstName} {person.LastName}";
                 }
+
                 
-            }
+                // set the first char to the current person's firstname character each loop
+                char firstChar = person.FirstName.ToCharArray()[0];
+      
+                if (firstChar == 'M')
+                {
+                    namesMList.Add($"{person.FirstName} {person.LastName}");
+
+                }
+
+
+                if (person.FavoriteColour == "Blue")
+                {
+                    likesBlueList.Add($"{person.FirstName} {person.LastName}");
+                }
+
+
+
+            }// foreach loop ends
+
+
+
+
+            // PRINTING RESULTS
+            
+            //print average age
+            ageAve = ageSum / personList.Count; //[1]
+            Console.WriteLine("the average age of the people in the list is " + ageAve.ToString());
+     
             // print the oldest and youngest
             Console.WriteLine("The youngest person is " + youngestPerson + " at age " + ageMin);
             Console.WriteLine("The oldest person is " + oldestPerson +  " at age " + ageMax);
 
             // names of the people whos first name start with M
-
-            //char firstChar = personList[0].FirstName.ToCharArray()[0];
-            //Console.WriteLine(firstChar=='M');
-            foreach (Person person in personList)
+            foreach(string name in namesMList)
             {
-                char firstChar = person.FirstName.ToCharArray()[0];
-                if (firstChar == 'M')
-                {
-                    Console.WriteLine($"{person.FirstName} {person.LastName}'s first name starts with the letter M.");
-                }
+                Console.WriteLine(name + "'s first name starts with the Lettter M.");
+            }
+            
+            // who likes blue
+            foreach(string name in likesBlueList)
+            {
+                Console.WriteLine(name + " likes the colour Blue.");
             }
 
-            // person info of the person that likes the colour blue
-            foreach(Person person in personList)
-            {
-                if(person.FavoriteColour == "Blue")
-                {
-                    Console.WriteLine($"{person.FirstName} {person.LastName} likes the colour Blue.");
-                }
-            }
+
+
+
 
 
             //References
